0e6a04926fa1ea01dd36789599783858a3400532
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-05-30 20:28:17 -0300

    fixed include and header ex01.

--- ex01/ft_print_alphabet.c
+++ ex01/ft_print_alphabet.c
@@ -6,16 +6,14 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/26 17:46:05 by dande-je          #+#    #+#             */
-/*   Updated: 2023/05/28 14:07:23 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/05/30 20:26:53 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ex01_ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+void	ft_print_alphabet(void);
+void	ex01_ft_putchar(char c);
 
 void	ft_print_alphabet(void)
 {
@@ -27,4 +25,9 @@ void	ft_print_alphabet(void)
 		ex01_ft_putchar(letter);
 		letter++;
 	}
+}
+
+void	ex01_ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
