57c9577fb5d96ccfa013ce7e5d91a7cde090dfd5
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-05-30 21:38:03 -0300

    fixed remove comma in the end of the program.

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 13:30:25 by dande-je          #+#    #+#             */
-/*   Updated: 2023/05/30 20:18:00 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/05/30 21:35:26 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,11 @@ void	ft_verify_number(int col_0, int col_1)
 		col_d_1 = col_1 / 10;
 		col_u_1 = col_1 % 10;
 		ft_print_number(col_d_0, col_u_0, col_d_1, col_u_1);
+		if (!(col_0 == 98 && col_1 == 99))
+		{
+			ex06_ft_putchar(',');
+			ex06_ft_putchar(' ');
+		}
 	}
 }
 
@@ -63,8 +68,6 @@ void	ft_print_number(int col_d_0, int col_u_0, int col_d_1, int col_u_1)
 	ex06_ft_putchar(' ');
 	ex06_ft_putchar(col_d_1 + num_ascii);
 	ex06_ft_putchar(col_u_1 + num_ascii);
-	ex06_ft_putchar(',');
-	ex06_ft_putchar(' ');
 }
 
 void	ex06_ft_putchar(int c)
