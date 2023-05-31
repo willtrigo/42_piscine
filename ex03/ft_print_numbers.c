9624872ae4b8ec11fa569b623388c992295cbf49
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-05-30 21:23:25 -0300

    fixed include and header ex03.

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -6,11 +6,14 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 14:28:21 by dande-je          #+#    #+#             */
-/*   Updated: 2023/05/28 17:56:50 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/05/30 21:22:30 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_print_numbers(void);
+void	ex03_ft_putchar(char c);
 
 void	ex03_ft_putchar(char c)
 {
