6ed8f5f9aff5ac5f5e25e19b759ab4d0221e8272
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-01 09:31:08 -0300

    fix output when you use null value.

--- ex04/ft_is_negative.c
+++ ex04/ft_is_negative.c
@@ -6,18 +6,18 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 18:18:08 by dande-je          #+#    #+#             */
-/*   Updated: 2023/05/30 21:25:43 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/05/31 20:04:05 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		ft_if_negative(int n);
+int		ft_is_negative(int n);
 void	ex04_ft_putchar(char c);
 
-int	ft_if_negative(int n)
+int	ft_is_negative(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 		ex04_ft_putchar('N');
 	else
 		ex04_ft_putchar('P');
