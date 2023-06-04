b33a68f90884de7aeaf6e1f55a5ba73df5f40778
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-04 16:47:40 -0300

    apply modifications and create grid.c.

--- ft_str.c
+++ ft_str.c
@@ -6,16 +6,76 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 21:55:49 by dande-je          #+#    #+#             */
-/*   Updated: 2023/06/03 21:56:26 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/06/04 16:35:28 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_str_len(char *str)
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+int	ft_str_len(char *str)
 {
-	int counter;
+	int	counter;
 
 	counter = 0;
 	while (str[counter] != '\0')
 		counter++;
-	return counter;
+	return (counter);
+}
+
+void	ft_print_number(int num)
+{
+	int	num_ascii;
+
+	num_ascii = 48;
+	if (num == -2147483647)
+		write (1, "-2147483647", 11);
+	else if (num > 0)
+	{
+		if (num < 10)
+			ft_putchar(num + num_ascii);
+		else
+		{
+			ft_print_number(num / 10);
+			ft_putchar((num % 10) + num_ascii);
+		}
+	}
+	else
+	{
+		ft_putchar('-');
+		ft_print_number(num);
+	}
+}
+
+void	ft_message_error(void)
+{
+	write(1, "Error\n", 6);
+}
+
+void	ft_print_str(char c, int i_end)
+{
+	int	i;
+
+	i = 0;
+	while (i < i_end)
+	{
+		write (1, &c, 1);
+		i++;
+	}
+}
+
+int	ft_check_char(char num)
+{
+	if ((num >= '1' && num <= '4'))
+	{
+		if ((num + 1) == ' ' || (num + 1) == '\0')
+			return (0);
+	}
+	else if (num == ' ')
+	{
+		if ((num + 1) >= '1' && (num + 1) <= '4')
+			return (0);
+	}
+	return (1);
 }
--- main.c
+++ main.c
@@ -3,106 +3,107 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: almarcos <almarcos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 16:20:46 by gmendonc          #+#    #+#             */
-/*   Updated: 2023/06/04 15:10:34 by almarcos         ###   ########.fr       */
+/*   Updated: 2023/06/04 16:35:06 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_error()
+int		main(int argc, char *argv[]);
+int		ft_str_len(char *str);
+void	ft_print_number(int num);
+void	ft_message_error(void);
+void	ft_print_str(char c, int i_end);
+int		ft_input_is_valide(char *input);
+void	ft_print_grid(int grid[4][4]);
+void	ft_fill_grid_input(char *input, int grid[4][4]);
+int		ft_check_char(char num);
+
+int	main(int argc, char **argv)
 {
-    write(1, "Error\n", 6);
+	int	grid[4][4];
+
+	if (argc != 2)
+	{
+		ft_message_error();
+		return (0);
+	}
+	if (!ft_input_is_valide(argv[1]))
+		return (0);
+	ft_fill_grid_input(argv[1], grid);
+	ft_print_grid(grid);
+	return (0);
 }
 
-int ft_input_is_valide(char *input)
+int	ft_input_is_valide(char *input)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (input[i] != '\0')
-    {
-        if ((input[i] >= '1' && input[i] <= '4') && (input[i + 1] == ' ' || input[i + 1] == '\0'))
-            i++;
-        else if (input[i] == ' ' && (input[i + 1] >= '1' && input[i + 1] <= '4'))
-            i++;
-        else
-        {
-            ft_error();
-            return 0;
-        }
-    }
-
-    if (i != 31)
-    {
-        ft_error();
-        return 0;
-    }
-
-    return 1;
+	i = 0;
+	if (ft_str_len(input) != 31)
+	{
+		ft_message_error();
+		return (0);
+	}
+	else
+	{
+		while (input[i] != '\0')
+		{
+			if (ft_check_char(input[i]))
+				i++;
+			else
+			{
+				ft_message_error();
+				return (1);
+			}
+		}
+	}	
+	return (1);
 }
 
-void ft_print_grid(int grid[4][4])
+void	ft_print_grid(int grid[4][4])
 {
-    int c;
-    int l;
-    int item;
+	int	col;
+	int	line;
+	int	item;
 
-    c = 0;
-    while (c < 4)
-    {
-        l = 0;
-        while (l < 4)
-        {
-            item = grid[c][l];
-            write(1, &item, 1);
-            write(1, " ", 1);
-            l++;
-        }
-        write(1, "\n", 1);
-        c++;
-    }
+	col = 0;
+	while (col < 4)
+	{
+		line = 0;
+		while (line < 4)
+		{
+			item = grid[col][line];
+			ft_print_str(item, 1);
+			ft_print_str(' ', 1);
+			line++;
+		}
+		ft_print_str('\n', 1);
+		col++;
+	}
 }
 
-void ft_fill_grid_input(char *input, int grid[4][4])
+void	ft_fill_grid_input(char *input, int grid[4][4])
 {
-    int c;
-    int l;
-    int iterator_input;
+	int	col;
+	int	line;
+	int	iterator_input;
 
-    iterator_input = 0;
-    c = 0;
-    while (c < 4)
-    {
-        l = 0;
-        while (l < 4)
-        {
-            if (input[iterator_input] == ' ')
-                iterator_input++;
-            grid[c][l] = input[iterator_input++];
-            l++;
-        }
-        c++;
-    }
-}
-
-int main(int argc, char **argv)
-{
-    int grid[4][4];
-
-    if (argc != 2)
-    {
-        ft_error();
-        return 0;
-    }
-
-    if (!ft_input_is_valide(argv[1]))
-        return 0;
-
-    ft_fill_grid_input(argv[1], grid);
-    ft_print_grid(grid);
-
-    return 0;
+	iterator_input = 0;
+	col = 0;
+	while (col < 4)
+	{
+		line = 0;
+		while (line < 4)
+		{
+			if (input[iterator_input] == ' ')
+				iterator_input++;
+			grid[col][line] = input[iterator_input++];
+			line++;
+		}
+		col++;
+	}
 }
