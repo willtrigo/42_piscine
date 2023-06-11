e6582972e7196cc520928261a4888d9837395471
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-11 09:02:49 -0300

    Done ex01.

---
+++ ex01/ft_strncmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/11 08:18:53 by dande-je          #+#    #+#             */
+/*   Updated: 2023/06/11 08:58:35 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n);
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	while(--n != '\0')
+	{
+		if (n == 0)
+			return (0);
+		if (*s1 != *s2++)
+			return (*(unsigned char *)s1 - *(unsigned char *)--s2);
+		if (*s1++ == '\0')
+			break ;
+	}
+	return (0);
+}
