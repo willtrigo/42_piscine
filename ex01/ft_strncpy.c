14ba6e4b337cc6f39bf3c8bc66c9a5760b70352b
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-13 14:02:55 -0300

    Fix logic ex01.

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,38 +6,26 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 19:30:27 by dande-je          #+#    #+#             */
-/*   Updated: 2023/06/12 22:54:57 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/06/13 13:18:09 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n);
-int		ft_str_len_ex01(char *str);
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
-	int	len;
+	unsigned int	i;
 
 	i = 0;
-	len = ft_str_len_ex01(dest);
-	while (((unsigned int)i) < n)
+	while (src[i] && i <n)
 	{
-		if (i < len)
-			dest[i] = src[i];
-		else
-			break ;
+		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-int	ft_str_len_ex01(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
-	return (i);
+	}
+	return (dest);
 }
