294a9d0494aa0cc2bad82ae30ce413d26fcfcce2
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-12 21:55:46 -0300

    Done ex04.

---
+++ ex04/ft_strstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/12 20:32:19 by dande-je          #+#    #+#             */
+/*   Updated: 2023/06/12 21:52:38 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find);
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	char *c_check;
+	char *to_find_check;
+
+	to_find_check = to_find;
+	if (to_find_check == 0)
+		return (str);
+	while (*str)
+	{
+		if (*str == *to_find)
+		{
+			c_check = str;
+			while (1)
+			{
+				if (*to_find_check == 0)
+					return (str);
+				if (*to_find_check != *c_check)
+					break ;
+				to_find_check++;
+				c_check++;
+			}
+			to_find_check = to_find;
+		}
+		str++;
+	}
+	return (0);
+}
