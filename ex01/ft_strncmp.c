59a08cb654d024605b9429e461a22b71bbbce662
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-06-13 15:47:26 -0300

    fix condition logic, line 22.

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/11 08:18:53 by dande-je          #+#    #+#             */
-/*   Updated: 2023/06/12 14:49:11 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/06/13 15:45:53 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n);
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while (--n != 0)
+	unsigned int i;
+
+	i = 0;
+	if (n != 0)
 	{
-		if (n == 0)
-			return (0);
-		if (*s1 != *s2++)
-			return (*(unsigned char *)s1 - *(unsigned char *)--s2);
-		if (*s1++ == '\0')
-			break ;
+		while (s1[i] && (s1[i] == s2[i]) && (i < (n - 1)))
+			i++;
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	}
 	return (0);
 }
